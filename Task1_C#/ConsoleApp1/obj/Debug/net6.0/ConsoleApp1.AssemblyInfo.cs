ccafee082cc98922a7eeeae6dd6816e36f726032
Shady-Mo <shady.mohamed7899@gmail.com>
2025-04-13 22:26:43 +0200

    C# Tasks

--- Task1_C#/ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ Task1_C#/ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+613c96c31eb96d94859f85dd39fccb34429cd640")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
